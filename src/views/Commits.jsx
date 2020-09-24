a046e9d526b5f225cebdd02137f8512d716f88e8
Firmino Changani <firmino.changani@gmail.com>
2020-09-24 22:46:30 +0100

    feat: Import commit component

--- src/views/Commits.jsx
+++ src/views/Commits.jsx
@@ -1,23 +1,48 @@
-import React, { useEffect } from "react";
 import { connect } from "react-redux";
+import React, { useEffect } from "react";
+import Commit from "../components/Commit";
+import { getCommits } from "../store/thunks";
+import { useLocation } from "react-router-dom";
+import styled from "styled-components";
 
-const Home = ({ commits = [] }) => {
+const useQuery = () => {
+  return new URLSearchParams(useLocation().search);
+};
+
+const Container = styled.div`
+  width: 90%;
+  margin: 0 auto;
+`;
+
+const Commits = ({ commits = [], loadCommits }) => {
+  const query = useQuery().get("q");
   useEffect(() => {
-    console.log(commits);
-  }, [commits]);
+    loadCommits(query);
+  }, [loadCommits, query]);
 
   return (
-    <>
+    <Container>
       Commits
       {commits.map((commit) => (
-        <div>{commit.url}</div>
+        <Commit
+          id={commit.sha}
+          key={commit.sha}
+          url={commit.html_url}
+          title={commit.commit.message}
+          repository={commit.repository.name}
+          author={commit.commit.author.name}
+        />
       ))}
-    </>
+    </Container>
   );
 };
+
+const mapDispatchToProps = (dispatch) => ({
+  loadCommits: (query) => dispatch(getCommits(query)),
+});
 
 const mapStateToProps = (state) => ({
   commits: state.search.commits,
 });
 
-export default connect(mapStateToProps, null)(Home);
+export default connect(mapStateToProps, mapDispatchToProps)(Commits);
